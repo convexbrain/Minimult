6060d52e1da2c549b2abc2c5bf06c1bcbffe3bb9
convexbrain <convexbrain@gmail.com>
2019-11-01 01:09:17 +0900

    created STM32F746NG

---
+++ examples/STM32F746NG/README.md
@@ -0,0 +1,20 @@
+# STM32F746NG
+
+## Device
+
+* STM32F746NG by STMicroelectronics
+  * https://www.st.com/ja/microcontrollers-microprocessors/stm32l053r8.html
+  * ARM Cortex-M7
+    * up to 216 MHz
+    * 1 MByte FLASH
+    * 320 kByte RAM
+  * STM32F7 System View Description
+    * https://www.st.com/resource/en/svd/stm32f7_svd.zip
+
+## Board and Debugger
+
+* 32F746GDISCOVERY by STMicroelectronics
+  * https://www.st.com/ja/evaluation-tools/32f746gdiscovery.html
+  * Manual: https://www.st.com/resource/en/user_manual/dm00190424.pdf
+* Embedded ST-LINK/V2-1
+  * Convert into J-Link: https://www.segger.com/products/debug-probes/j-link/models/other-j-links/st-link-on-board/
---
+++ examples/STM32F746NG/led_blink/src/main.rs
@@ -0,0 +1,168 @@
+#![no_main]
+#![no_std]
+
+use cortex_m::asm;
+use cortex_m::peripheral::NVIC;
+use cortex_m::Peripherals;
+
+use cortex_m_rt::entry;
+use cortex_m_rt::exception;
+
+extern crate panic_semihosting;
+
+use stm32f7xx_hal::{
+    device::{self, interrupt, Interrupt},
+    prelude::*, gpio::*, timer::*};
+
+use minimult_cortex_m::*;
+
+//
+
+const CLOCK: u32 = 216_000_000;
+struct Toggle(u32, u32);
+
+#[entry]
+fn main() -> ! {
+    let peri = device::Peripherals::take().unwrap();
+
+    let mut rcc = peri.RCC.constrain();
+    let clocks = rcc.cfgr.sysclk(216.mhz()).freeze();
+
+    // ----- ----- ----- ----- -----
+
+    let mut mem = Minimult::mem::<[u8; 4096]>();
+    let mut mt = Minimult::new(&mut mem, 3);
+
+    // ----- ----- ----- ----- -----
+
+    let gpioi = peri.GPIOI.split();
+    let pi1 = gpioi.pi1.into_push_pull_output();
+
+    // ----- ----- ----- ----- -----
+
+    let mut timer2 = Timer::tim2(peri.TIM2, 1.hz(), clocks, &mut rcc.apb1);
+    timer2.listen(Event::TimeOut);
+    let tim2cnt = 2; // NOTE: ??? need to adjust to make it 1hz
+
+    unsafe { NVIC::unmask(Interrupt::TIM2) }
+
+    // ----- ----- ----- ----- -----
+
+    let cmperi = Peripherals::take().unwrap();
+    let mut syst = cmperi.SYST;
+    syst.set_clock_source(cortex_m::peripheral::syst::SystClkSource::Core);
+    syst.set_reload(CLOCK / 16 - 1);
+    syst.clear_current();
+    syst.enable_counter();
+    syst.enable_interrupt();
+    let systcnt = 36; // 36/16=2.25 sec
+
+    // ----- ----- ----- ----- -----
+
+    let cnt0 = CLOCK / 8;
+    let div0 = 1;
+    let cnt1 = CLOCK;
+    let div1 = 4;
+
+    /* using message queue
+     */
+    let mut q = mt.msgq::<Toggle>(4);
+    let (snd, rcv) = q.ch();
+
+    let s_snd = mt.share(snd);
+    let sc_snd0 = s_snd.ch();
+    let sc_snd1 = s_snd.ch();
+
+    mt.register(0, 1, 256, || _led_tim0(tim2cnt, sc_snd0, cnt0, div0));
+    mt.register(1, 1, 256, || _led_tim1(systcnt, sc_snd1, cnt1, div1));
+    mt.register(2, 2, 256, || _led_tgl(pi1, rcv)); // blink and pause
+
+    {   // must be error in terms of lifetime
+        //core::mem::drop(mem);
+        //core::mem::drop(q);
+        //core::mem::drop(s_snd);
+        //core::mem::drop(sc_snd0);
+        //core::mem::drop(sc_snd1);
+        //core::mem::drop(rcv);
+    }
+    
+    // ----- ----- ----- ----- -----
+
+    mt.run()
+}
+
+fn _led_tgl(mut pi1: gpioi::PI1<Output<PushPull>>, mut rcv: MTMsgReceiver<Toggle>)
+{
+    let mut tgl = Toggle(CLOCK / 16, 1);
+
+    loop {
+        for _ in 0..tgl.1 {
+            pi1.set_high().unwrap();
+
+            asm::delay(tgl.0 / 4 / tgl.1);
+
+            pi1.set_low().unwrap();
+
+            asm::delay(tgl.0 / 4 / tgl.1);
+        }
+
+        pi1.set_low().unwrap();
+
+        asm::delay(tgl.0 / 2);
+
+        tgl = rcv.receive();
+    }
+}
+
+fn _led_tim0(timcnt: u32, sc_snd: MTSharedCh<MTMsgSender<Toggle>>, cnt: u32, div: u32)
+{
+    loop {
+        for _ in 0..timcnt {
+            Minimult::idle();
+
+            //
+
+            // NOTE: ??? unsafe way to clear UIF
+            let tim2_sr = 0x4000_0010 as *mut u32;
+            unsafe {
+                let r = tim2_sr.read_volatile();
+                tim2_sr.write_volatile(r & 0xfffffffe);
+            }
+            NVIC::unpend(Interrupt::TIM2);
+            unsafe { NVIC::unmask(Interrupt::TIM2) }
+        }
+
+        //
+
+        let mut snd = sc_snd.touch();
+        snd.send(Toggle(cnt, div));
+    }
+}
+
+fn _led_tim1(timcnt: u32, sc_snd: MTSharedCh<MTMsgSender<Toggle>>, cnt: u32, div: u32)
+{
+    loop {
+        for _ in 0..timcnt {
+            Minimult::idle();
+        }
+
+        //
+
+        let mut snd = sc_snd.touch();
+        snd.send(Toggle(cnt, div));
+    }
+}
+
+#[interrupt]
+fn TIM2()
+{
+    NVIC::mask(Interrupt::TIM2);
+    
+    Minimult::kick(0);
+}
+
+#[exception]
+fn SysTick()
+{
+    Minimult::kick(1);
+}
